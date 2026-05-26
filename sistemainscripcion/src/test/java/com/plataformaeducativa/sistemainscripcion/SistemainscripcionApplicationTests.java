f14d8471b4ef24320426e72feb1749d53cb71bd3
Nicole Plaza <nico.plaza@duocuc.cl>
2026-05-25 21:12:21 -0400

    primer commit

---
+++ sistemainscripcion/src/test/java/com/plataformaeducativa/sistemainscripcion/SistemainscripcionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plataformaeducativa.sistemainscripcion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemainscripcionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
